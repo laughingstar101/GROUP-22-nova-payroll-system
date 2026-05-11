b3c20d86a71be87d8efdbfc1b22b19211b224701
laughingstar101 <shamhyqal@gmail.com>
2026-05-12 00:08:05 +0800

    add login container

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
-import { Routes, Route } from "react-router-dom";
 import CompanyRegister from "./components/CompanyRegister";
-import CompanyLogin from './components/CompanyLogin';
-import EmployeeRegister from './components/EmployeeRegister'
-import Dashboard from "./components/Dashboard";
 
 export default function App() {
     return (
-        <Routes>
-            <Route path="/" element={<CompanyRegister/>}></Route>
-            <Route path="/employeeRegister" element={<EmployeeRegister/>}></Route>
-            <Route path="/companyLogin" element={<CompanyLogin/>}></Route>
-            <Route path="/dashboard" element={<Dashboard/>}></Route>
-        </Routes>
+        <CompanyRegister/>
     )
 }
