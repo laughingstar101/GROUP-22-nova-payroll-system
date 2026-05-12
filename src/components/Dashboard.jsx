777417952a6a4ad8c241207c5ade652732c26a74
laughingstar101 <shamhyqal@gmail.com>
2026-05-12 19:12:38 +0800

    fetch company data in dashboard and add loader

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,6 +1,46 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { supabase } from "../utils/supabase/supabase";
+import TopBar from "./TopBar";
+
 export default function Dashboard() {
+    const [company, setCompany] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const navigate = useNavigate;
+
+    useEffect(() => {
+        const fetchCompany = async () => {
+            const { data: { user }} = await supabase.auth.getUser();
+            // if user dont exist
+            if (!user) { 
+                // go back to register
+                navigate("/");
+                return;
+            }
+            const { data, error } = await supabase
+                .from("Company")
+                .select("company_name, company_email, company_id")
+                .eq("company_id", user.id)
+                .single();
+            if (error) {
+                console.error("Error fetching company: ", error);
+                navigate("/");
+            } else {
+                setCompany(data);
+            }
+            setLoading(false);
+        };
+        fetchCompany();
+    }, [navigate]);
+
+    if (loading) {
+        return <div className="loader"></div>
+    }
+
     return (
-        <h1>Dashboard</h1>
-        // const { data: { user }}
-    )
+        <div>
+            <TopBar/>
+            <h1>Hello world</h1>
+        </div>
+    );
 }
