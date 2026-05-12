deceeb5919f0e9b4058a0efd56ec75adb640158f
laughingstar101 <shamhyqal@gmail.com>
2026-05-12 15:06:19 +0800

    refactoring to tailwind

--- src/router.jsx
+++ src/router.jsx
@@ -1,13 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 // import App from "./App";
 import EmployeeRegister from "./components/EmployeeRegister";
-import CompanyLogin from "./components/CompanyLogin";
 import Dashboard from "./components/Dashboard";
 import App from './App'
 
 export const router = createBrowserRouter([
     {path: "/", element: <App/>},
     {path: "/employeeRegister", element: <EmployeeRegister/>},
-    {path: "/companyLogin", element: <CompanyLogin/>},
     {path: "/dashboard", element: <Dashboard/>},
 ])  
