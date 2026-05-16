30bd5517b1a1654f4a9824a73c93f73ff9d5c614
laughingstar101 <shamhyqal@gmail.com>
2026-05-16 17:03:11 +0800

    responsiveness 2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,7 +17,7 @@ export default function Hero() {
                 alt="background"
             />
             
-            <div className='bg-primary-colour w-full z-10 flex justify-between items-center py-4 px-20 shadow-xl'>
+            <div className='bg-primary-colour w-full z-10 justify-between items-center py-4 px-20 shadow-xl md:flex hidden'>
                 <img src={logoImg} className="h-15 select-none" alt="logo" />
                 <button 
                     onClick={handleRegisterClick} 
@@ -27,7 +27,7 @@ export default function Hero() {
                 </button>
             </div>
             
-            <div className="flex-1 z-10 flex flex-col md:justify-center justify-normal items-baseline 2xl:mr-180 xl:mr-160 lg:mr-100 md:mr-70 2xl:w-150 md:w-110 w-full md:p-0 pr-60 pl-6 mt-20">
+            <div className="flex-1 z-10 flex flex-col md:justify-center justify-normal items-baseline 2xl:mr-180 xl:mr-160 lg:mr-100 md:mr-70 2xl:w-150 md:w-110 w-full md:p-0 pr-60 pl-6 mt-12">
                 <span>
                     <span className='text-white text-shadow-sm text-shadow-black text-7xl font-hero! text-left'>n</span>
                     <span className='text-complementary-colour text-shadow-sm text-shadow-black text-7xl font-hero! text-left'>o</span>
@@ -38,6 +38,12 @@ export default function Hero() {
                 <p className='text-slate-300 text-xl font-hero! text-left mt-2'>
                     Use our system to launch your company's payroll management to new heights!
                 </p>
+                <button 
+                    onClick={handleRegisterClick} 
+                    className='md:hidden flex mt-6 hover:cursor-pointer text-complementary-colour text-xl font-hero! uppercase bg-secondary-colour2 py-2 px-4 rounded-3xl hover:scale-105 hover:shadow-md hover:shadow-black transition-scale duration-100'
+                >
+                    register
+                </button>
             </div>
         </div>
     );
