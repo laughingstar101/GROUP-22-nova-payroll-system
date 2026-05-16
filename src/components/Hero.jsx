e762d6cfc95014fde078de01dafee428db2288da
laughingstar101 <shamhyqal@gmail.com>
2026-05-16 19:16:36 +0800

    change letter spacing

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -34,8 +34,8 @@ export default function Hero() {
                     <span className='text-white text-shadow-sm text-shadow-black text-7xl font-hero! text-left'>v</span>
                     <span className='text-white text-shadow-sm text-shadow-black text-7xl font-hero! text-left'>a</span>
                 </span>
-                <p className='text-complementary-colour text-shadow-sm text-shadow-black text-6xl font-hero! text-left'>Payroll System</p>
-                <p className='text-white text-xl font-hero! text-left mt-2'>
+                <p className='text-complementary-colour text-shadow-sm text-shadow-black text-6xl font-hero! text-left tracking-wider'>Payroll System</p>
+                <p className='text-white text-xl font-hero! text-left mt-2 tracking-wider'>
                     Use our system to launch your company's payroll management to new heights!
                 </p>
                 <button 
