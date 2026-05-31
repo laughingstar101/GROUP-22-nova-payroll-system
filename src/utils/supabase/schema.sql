3dcdf2da0e600aa9d7eb2c1e999229393dc29a40
laughingstar101 <shamhyqal@gmail.com>
2026-05-31 19:32:46 +0800

    Update schema.sql

--- src/utils/supabase/schema.sql
+++ src/utils/supabase/schema.sql
@@ -1,19 +1,20 @@
 -- WARNING: This schema is for context only and is not meant to be run.
 -- Table order and constraints may not be valid for execution.
 
-CREATE TABLE public.Attendance (
+CREATE TABLE public."Attendance" (
   id uuid NOT NULL DEFAULT gen_random_uuid(),
   employee_id uuid NOT NULL,
   check_in timestamp with time zone NOT NULL,
   check_out timestamp with time zone,
   date date NOT NULL,
-  status text DEFAULT 'PENDING'::text,
+  status_hours text DEFAULT 'PENDING'::text,
   created_at timestamp with time zone NOT NULL DEFAULT now(),
   work_duration interval DEFAULT '00:00:00'::interval,
+  status_on_time text DEFAULT ''::text,
   CONSTRAINT Attendance_pkey PRIMARY KEY (id),
   CONSTRAINT Attendance_employee_id_fkey FOREIGN KEY (employee_id) REFERENCES public.Employee(id)
 );
-CREATE TABLE public.Company (
+CREATE TABLE public."Company" (
   id uuid NOT NULL DEFAULT gen_random_uuid(),
   company_email text NOT NULL,
   company_name text NOT NULL,
@@ -24,7 +25,7 @@ CREATE TABLE public.Company (
   CONSTRAINT Company_pkey PRIMARY KEY (id),
   CONSTRAINT Company_company_id_fkey FOREIGN KEY (company_id) REFERENCES auth.users(id)
 );
-CREATE TABLE public.Employee (
+CREATE TABLE public."Employee" (
   id uuid NOT NULL DEFAULT gen_random_uuid(),
   employee_email text NOT NULL,
   employee_name text NOT NULL,
@@ -32,10 +33,11 @@ CREATE TABLE public.Employee (
   employee_company uuid,
   created_at timestamp with time zone DEFAULT now(),
   profile_image text,
+  qr_token text UNIQUE,
   CONSTRAINT Employee_pkey PRIMARY KEY (id),
   CONSTRAINT Employee_employee_company_fkey FOREIGN KEY (employee_company) REFERENCES public.Company(id)
 );
-CREATE TABLE public.Leave (
+CREATE TABLE public."Leave" (
   id uuid NOT NULL DEFAULT gen_random_uuid(),
   created_at timestamp with time zone NOT NULL DEFAULT now(),
   employee_id uuid NOT NULL,
@@ -45,7 +47,7 @@ CREATE TABLE public.Leave (
   CONSTRAINT Leave_pkey PRIMARY KEY (id),
   CONSTRAINT Leave_employee_id_fkey FOREIGN KEY (employee_id) REFERENCES public.Employee(id)
 );
-CREATE TABLE public.Leave Type (
+CREATE TABLE public."Leave Type" (
   id uuid NOT NULL DEFAULT gen_random_uuid(),
   leave_name text NOT NULL,
   description text,
